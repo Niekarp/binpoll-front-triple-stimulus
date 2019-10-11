448022f9f963c553d2a0d2a670a8ff6e2989d385
Oskar Dadan <skrzynkanof@gmail.com>
2019-10-12 01:08:46 +0200

    fix(debug): change url to api

--- src/app/debug/auth/auth.service.ts
+++ src/app/debug/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { of, Observable } from 'rxjs';
 import { timeout, tap, delay } from 'rxjs/operators';
 
 import md5 from 'js-md5';
+import { ConfigService } from 'src/app/services/config/config.service';
 // const md5 = require('js-md5');
 
 @Injectable({
@@ -12,12 +13,12 @@ import md5 from 'js-md5';
 export class AuthService {
   public isLoggedIn = false;
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, public config: ConfigService) { }
 
   public login(password: string): Observable<boolean> {
     // return of(true).pipe(delay(3000), tap(val => this.isLoggedIn = true));
     const md5Password = md5(password);
-    return this.http.post<boolean>('http://localhost/api/debug/', { key: md5Password }).pipe(
+    return this.http.post<boolean>(`${this.config.urlConfig.apiUrl}/debug/`, { key: md5Password }).pipe(
       tap(success => this.isLoggedIn = true)
     );
   }
