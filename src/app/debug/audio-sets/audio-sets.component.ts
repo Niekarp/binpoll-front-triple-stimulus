a5f547566c33f65a68eac2a8c85ccc562bd8541a
Oskar Dadan <skrzynkanof@gmail.com>
2019-10-12 02:31:30 +0200

    impr(debug): the refresh button

--- src/app/debug/audio-sets/audio-sets.component.ts
+++ src/app/debug/audio-sets/audio-sets.component.ts
@@ -15,47 +15,57 @@ export class AudioSetsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.doRefresh();
+
+    // setInterval(() => {
+    
+    // }, this.REFRESH_INTERVAL);
+  }
+
+  refresh() {
+    this.doRefresh();
+  }
+
+  doRefresh() {
     const $mainContainer = document.getElementById('audio-sets-container');
 
-    setTimeout(() => {
-      $mainContainer.innerHTML = '';
+    $mainContainer.innerHTML = '';
 
-      const response = fetch(this.REQUEST_URL, { mode: 'cors' }).then(response => {
-        response.json().then(data => {
-          const audioSets = data;
-          let j = 0;
-          for (let i = 0; i < 156; ++i) {
-            const $audioSet = document.createElement('div');
-            $audioSet.innerHTML = (i + 1).toString();
-            // $audioSet.classList.add('audio-set');
-            // color: white;
-            // margin: 2px;
-            // flex: 0 0 50px;
-            // text-align: center;
-            // transition: height 2s;
-            $audioSet.style.color = 'white';
-            $audioSet.style.margin = '2px';
-            $audioSet.style.marginBottom = '50px';
-            $audioSet.style.flex = '0 0 50px';
-            $audioSet.style.textAlign = 'center';
-            
-            if (audioSets[j].audioSet.id - 1 !== i) {
-              $audioSet.style.height = '80px';
-              $audioSet.style.backgroundColor = 'green';
-            } else if (audioSets[j].isLocked) {
-              $audioSet.style.height = '50px';
-              $audioSet.style.backgroundColor = 'brown';
-              ++j;
-            } else {
-              $audioSet.style.height = '20px';
-              $audioSet.style.backgroundColor = 'blue';
-              ++j
-            }
-            $mainContainer.append($audioSet);
+    const response = fetch(this.REQUEST_URL, { mode: 'cors' }).then(response => {
+      response.json().then(data => {
+        const audioSets = data;
+        let j = 0;
+        for (let i = 0; i < 156; ++i) {
+          const $audioSet = document.createElement('div');
+          $audioSet.innerHTML = (i + 1).toString();
+          // $audioSet.classList.add('audio-set');
+          // color: white;
+          // margin: 2px;
+          // flex: 0 0 50px;
+          // text-align: center;
+          // transition: height 2s;
+          $audioSet.style.color = 'white';
+          $audioSet.style.margin = '2px';
+          $audioSet.style.marginBottom = '50px';
+          $audioSet.style.flex = '0 0 50px';
+          $audioSet.style.textAlign = 'center';
+          
+          if (audioSets[j].audioSet.id - 1 !== i) {
+            $audioSet.style.height = '80px';
+            $audioSet.style.backgroundColor = 'green';
+          } else if (audioSets[j].isLocked) {
+            $audioSet.style.height = '50px';
+            $audioSet.style.backgroundColor = 'brown';
+            ++j;
+          } else {
+            $audioSet.style.height = '20px';
+            $audioSet.style.backgroundColor = 'blue';
+            ++j
           }
-        })
-      });
-    }, this.REFRESH_INTERVAL);
+          $mainContainer.append($audioSet);
+        }
+      })
+    });
   }
 
 }
